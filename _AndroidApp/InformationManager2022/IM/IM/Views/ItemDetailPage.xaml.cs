ab79d3a7b39a5f4cfa56176f656e69334217bd0f
Kagan-Ditlevsen <kagan-ditlevsen@live.dk>
2022-09-06 05:14:28 +0200

    .

---
+++ _AndroidApp/InformationManager2022/IM/IM/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using IM.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace IM.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
